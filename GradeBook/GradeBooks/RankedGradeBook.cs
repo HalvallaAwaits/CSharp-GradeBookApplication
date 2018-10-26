7900bea6fa9060631decc2c0ba04486d55cafd19
Reimbold, Joseph <JReimbold@geico.com>
2018-10-26 14:28:10 -0400

    Completed Module - Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,9 +14,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
                 throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work");
-            }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(student => student.AverageGrade).Select(student => student.AverageGrade).ToList();
@@ -30,6 +28,28 @@ namespace GradeBook.GradeBooks
             if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
